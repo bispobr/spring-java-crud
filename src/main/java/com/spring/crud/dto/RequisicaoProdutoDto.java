41efc3cf272090920c6e20b926af57774f37baee
bispobr <156684382+bispobr@users.noreply.github.com>
2026-01-11 13:48:32 -0300

    refactor: remoção de campo não usado

--- src/main/java/com/spring/crud/dto/RequisicaoProdutoDto.java
+++ src/main/java/com/spring/crud/dto/RequisicaoProdutoDto.java
@@ -3,5 +3,5 @@ package com.spring.crud.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record RequisicaoProdutoDto(String id, @NotBlank String nome, @NotNull Integer preco) {
+public record RequisicaoProdutoDto(@NotBlank String nome, @NotNull Integer preco) {
 }
